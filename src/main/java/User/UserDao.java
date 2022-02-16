9a3e8225595af12324e5da5d6d4ca9ac5a13d551
Domino419 <96913280+Domino419@users.noreply.github.com>
2022-02-16 22:37:45 +0900

    Add files via upload

--- src/main/java/User/UserDao.java
+++ src/main/java/User/UserDao.java
@@ -2,8 +2,10 @@ package User;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.*;
-import oracle.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.io.UnsupportedEncodingException;
 
 
  
@@ -26,7 +28,7 @@ public class UserDao {
 		 
 	   
 			public int login(String userID, String userPassword) throws ClassNotFoundException, SQLException {
-
+			  
 			/*
 			 * Class.forName("oracle.jdbc.driver.OracleDriver"); conn =
 			 * DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE","web_user",
@@ -56,11 +58,24 @@ public class UserDao {
 		String SQL = "INSERT INTO bbs VALUES (?,?,?,?,?)" ;
 		try {
 			pstmt = conn.prepareStatement(SQL);
-			pstmt.setString(1,user.getUserID());
-			pstmt.setString(2,user.getUserPassword());
-			pstmt.setString(3,user.getUserName());
-			pstmt.setString(4,user.getUserGender());
-			pstmt.setString(5,user.getUserEmail());
+			String UserID;
+			UserID = new String(user.getUserID().getBytes("iso-8859-1"),"euc-kr");
+	        String UserPassword;
+	        UserPassword = new String(user.getUserPassword().getBytes("iso-8859-1"),"euc-kr");
+	        String UserName;
+	        UserName = new String(user.getUserName().getBytes("iso-8859-1"),"euc-kr");
+	        String UserGender;
+	        UserGender = new String(user.getUserGender().getBytes("iso-8859-1"),"euc-kr");
+	        String Email;
+	        Email = new String(user.getUserEmail().getBytes("iso-8859-1"),"euc-kr");
+	        
+	      
+	        
+	        pstmt.setString(1,UserID);
+			pstmt.setString(2,UserPassword);
+			pstmt.setString(3,UserName);
+			pstmt.setString(4,UserGender);
+			pstmt.setString(5,Email);
 			return pstmt.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -68,6 +83,20 @@ public class UserDao {
 		return -1; 
 	}
 }
-		
-			
+	        
+//			pstmt = conn.prepareStatement(SQL);
+//			pstmt.setString(1,user.getUserID());
+//			pstmt.setString(2,user.getUserPassword());
+//			pstmt.setString(3,user.getUserName());
+//			pstmt.setString(4,user.getUserGender());
+//			pstmt.setString(5,user.getUserEmail());
+//			return pstmt.executeUpdate();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return -1; 
+//	}
+//}
+//		
+//			
 
