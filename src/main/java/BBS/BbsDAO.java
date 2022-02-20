fb4ce177fcf085b75a29075bc073dfaba462b033
Domino419 <96913280+Domino419@users.noreply.github.com>
2022-02-20 23:56:21 +0900

    Update BbsDAO.java

--- src/main/java/BBS/BbsDAO.java
+++ src/main/java/BBS/BbsDAO.java
@@ -13,7 +13,7 @@ public class BbsDAO {
   ResultSet rs;                    //결과값 받아오는 객체  	
 
   public BbsDAO() { try{ Class.forName("oracle.jdbc.driver.OracleDriver");
-  conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE","web_user","pass"); 
+  conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE","web_user","~~~~"); 
   }catch 
 	  (Exception e)
 			  { e.printStackTrace() ;    //connection부터 이까지 한 덩어리//
