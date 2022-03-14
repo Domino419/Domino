ac536fcfc545c647a8140336e2ee8477356dca38
Domino419 <96913280+Domino419@users.noreply.github.com>
2022-03-14 23:29:11 +0900

    Add files via upload
    로그인 쿠키 기능 구현

--- src/main/java/User/UserDao.java
+++ src/main/java/User/UserDao.java
@@ -5,7 +5,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.io.UnsupportedEncodingException;
 
 
  
@@ -16,43 +15,38 @@ public class UserDao {
 
 	
 		
-		  public UserDao() { 
-			  try
-		  { 
-				  Class.forName("oracle.jdbc.driver.OracleDriver");
-		          conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE","web_user","~~~~"); 
-		  }catch (Exception e)
-		  { e.printStackTrace() ;
-		    }
-		  }
-		 
+		public UserDao() {
+			try {
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE", "web_user", "pass");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	   
-			public int login(String userID, String userPassword) throws ClassNotFoundException, SQLException {
-			  
-			/*
-			 * Class.forName("oracle.jdbc.driver.OracleDriver"); conn =
-			 * DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE","web_user",
-			 * "pass");
-			 */
+		
+
+		public int login(String userID, String userPassword) throws ClassNotFoundException, SQLException {
+
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE","web_user", "pass");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE", "web_user", "pass");
 			String sql = "select userPassword from bbs where userID = ?";
-		//	System.out.println(sql); 
-				pstmt = conn.prepareStatement(sql); 
-				pstmt.setString(1, userID); 
-				rs = pstmt.executeQuery(); 
-			//	System.out.println(rs);
-				if(rs.next()) {
-					if(rs.getString(1).equals(userPassword)) {
-						return 1; 
-					}else
-						return 0; 
-				}
-				return -1; 
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				if (rs.getString(1).equals(userPassword)) {
+
+					return 1;
+				} else
+					return 0;
+			}
+			return -1;
+
+		}
 			
-			//return -2; //DB 
-				
-		}		
+			
+	
 				
 	public int join(User user) {
 		String SQL = "INSERT INTO bbs VALUES (?,?,?,?,?)" ;
