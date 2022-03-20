8257d1cdd452a5bcf1b5bef43bd5092c4f6d1def
Domino419 <96913280+Domino419@users.noreply.github.com>
2022-03-20 22:52:53 +0900

    Add files via upload
    달력 dto dao 수정

--- src/main/java/Tableocl/CalendarDao.java
+++ src/main/java/Tableocl/CalendarDao.java
@@ -1,10 +1,15 @@
 package Tableocl;
 
+import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+
 import java.util.ArrayList;
+
+
+
 
 
 public class CalendarDao {
@@ -13,7 +18,7 @@ public class CalendarDao {
 	  ResultSet rs;                    //결과값 받아오는 객체  	
 
 	  public CalendarDao() { try{ Class.forName("oracle.jdbc.driver.OracleDriver");
-	  conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE","web_user","~~~~"); 
+	  conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE","web_user","pass"); 
 	  }catch 
 		  (Exception e)
 				  { e.printStackTrace() ;    //connection부터 이까지 한 덩어리//
@@ -25,49 +30,63 @@ public class CalendarDao {
 		    		+ "VALUES (?, ?, ?, ?)";
 		    try {
 		        PreparedStatement pstmt = conn.prepareStatement(SQL);
-		        
-				/*
-				 * //String MemoYear; MemoYear = new
-				 * String(MemoYear.getBytes("iso-8859-1"),"euc-kr"); // String MemoMonth = 0;
-				 * MemoMonth = new String(MemoMonth.getBytes("iso-8859-1"),"euc-kr"); // String
-				 * MemoDay = 0; MemoDay = new String(MemoDay.getBytes("iso-8859-1"),"euc-kr");
-				 * // String MemoContents = 0;
-				 */		        
-		        //MemoContents = new String(MemoContents.getBytes("iso-8859-1"),"euc-kr");
-		        
+
 		        pstmt.setInt(1, Integer.parseInt(MemoYear));
 		        pstmt.setInt(2, Integer.parseInt(MemoMonth));
 		        pstmt.setInt(3, Integer.parseInt(MemoDay));
 		        pstmt.setString(4, MemoContents);
-				/* pstmt.setInt(6, 1); */
 		        return pstmt.executeUpdate();
 		    } catch (Exception e) {
 		        e.printStackTrace();
+		 
+		    return -1;} // 데이터베이스 오류
 		    }
-		    return -1; // 데이터베이스 오류
+       
+
+			/*
+			 * 
+			 * SimpleDateFormat format1 = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");
+			 * SimpleDateFormat format2 = new SimpleDateFormat ( "yyyy년 MM월dd일 HH시mm분ss초");
+			 * 
+			 * String format_time1 = format1.format (System.currentTimeMillis()); String
+			 * format_time2 = format2.format (System.currentTimeMillis());
+			 */
+	
+  
+			public ArrayList<CalendarDto> getList(String y_, String m_) throws UnsupportedEncodingException   
+			{
+				String y = "year";
+	    		if(y_ != null && !y_.equals(""))
+	    			y = y_;
+	    		
+	    		String m = "";
+	    		if(m_ != null && !m_.equals(""))
+	    			m = m_;
+				
+				
+		    String SQL = "SELECT MEMOYEAR , MEMOMONTH, MEMODAY, MEMOCONTENTS FROM CALENDARMEMO WHERE MEMOYEAR in (?) AND MEMOMONTH IN (?)" ; 
+
+		    ArrayList<CalendarDto> list = new ArrayList<CalendarDto>();
+				try {
+					PreparedStatement pstmt = conn.prepareStatement(SQL);
+					pstmt.setString(1, y);
+				    pstmt.setString(2, m);
+					rs = pstmt.executeQuery();
+					
+					while (rs.next()) {
+						CalendarDto calendardto = new CalendarDto();  //리스트로 만들어서 반환 / 
+						
+						calendardto.setMemoYear(rs.getString(1));
+						calendardto.setMemoMonth(rs.getString(2));
+						calendardto.setMemoDay(rs.getString(3));
+						calendardto.setMemoContents(rs.getString(4)); 
+						list.add(calendardto);
+						
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				return list;
+			}
+
 		}
-	
-	
-	     public ArrayList<Calendar> getList()    
-	     {
-	     String SQL = "SELECT MEMOYEAR,MEMOMONTH, MEMODAY, MEMOCONTENTS FROM CALENDARMEMO; ";
-	     ArrayList<Calendar> list = new ArrayList<Calendar>();
-	     try {
-	         PreparedStatement pstmt = conn.prepareStatement(SQL);  
-	     /*    pstmt.setInt(1, (pageNumber - 1 ) * 10 + 1);
-	         pstmt.setInt(2, (pageNumber - 1 ) * 10 + 10);*/
-	         rs = pstmt.executeQuery();
-	         while (rs.next()) {
-	        	 Calendar Calendar = new Calendar();
-	             Calendar.setMemoYear(rs.getString(1));
-	             Calendar.setMemoMonth(rs.getString(2));
-	             Calendar.setMemoDay(rs.getString(3));
-	             Calendar.setMemoContents(rs.getString(4));
-	             list.add(Calendar);
-	         }
-	     } catch (Exception e) {
-	         e.printStackTrace();
-	     }
-	     return list; 
-	 }
-}     
